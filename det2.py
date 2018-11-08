7eee0aebd4f60663eb2e5e672f55075d5349aa90
FeyditRémi <remi.feydit@ynov.com>
2018-11-08 15:34:43 +0100

    Fonction det2 terminée et fonctionnel
    Fonction det2 fonctionnel

--- det2.py
+++ det2.py
@@ -1,5 +1,13 @@
 def det2(mat):
-    for i in mat:
-        print (i)
-        print (mat[1])
-print(det2([[-3,-3],[0,1]]))
+    #Test si la matrice est une matrice carré de taille 2
+    if len(mat)!=2 or len(mat[0])!=2 or len(mat[1])!=2:
+        return 'error'
+    #Calcul a*d
+    a=mat[0][0]*mat[1][1]
+    #Calcul b*c
+    b=mat[0][1]*mat[1][0]
+    #Calcul déterminant
+    determinant2=abs(a-b)
+    return determinant2
+    
+print(det2([[-3,4],[0,1]]))
