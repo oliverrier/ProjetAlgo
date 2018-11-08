70a87a79582b7eec066fad8a81803b29ddbf36df
Louis <louismaury33@gmail.com>
2018-11-08 15:42:41 +0100

    Première amélioration de la fonction reduit
    Tout est dans le titre.

---
+++ brouillon.py
@@ -0,0 +1,10 @@
+mat[[-3,-3,-4],[0,1,1],[4,3,4]] #Création de la matrice
+
+
+
+
+
+def reduit(mat,ligne,colonne):
+# La fonction va parcourir la matrice, I verticalement et J horizontalement.
+    for i in range 3:
+        for j in range 3:
