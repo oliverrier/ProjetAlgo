8be181c6e83c9fe4139d207bec4bae02070dc557
Olivier Verrier <olivier.verrier@ynov.com>
2018-11-08 15:01:42 +0100

    Création des objectifs
    Début de l'écriture des objectifs

---
+++ libmatrice.py
@@ -0,0 +1,11 @@
+'''
+Cette librairie a été créée afin d'inverser une matrice 3x3.
+Pour cela elle va utiliser plusieurs fonctions lui permettant de calculer cet inverse.
+Elle utilisera des fonctions permettant de calculer:
+    - le déterminant d'une matrice 2x2 appelée det2(mat)
+    - le déterminant d'une matrice 3x3 appelée det3(mat)
+    - la matrice 2x2 issue d'une matrice 3x3 réduite appelée reduit(mat,ligne,colonne)
+    - la comatrice d'une matrice appelée comatrice(mat)
+    - la transposée d'une matrice appelée transposée(mat)
+    - l'inverse d'une matrice appelée inverse(mat)
+'''
