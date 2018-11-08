11f160aff652d76b3bf1f14cb59b141189a647b0
Louis <louismaury33@gmail.com>
2018-11-08 16:33:12 +0100

    Finition de la fonction reduit
    J'ai terminé de programmer la fonction "reduit"

--- brouillon.py
+++ brouillon.py
@@ -1,10 +1,19 @@
-mat[[-3,-3,-4],[0,1,1],[4,3,4]] #Création de la matrice
+mat = [[-3,-3,-4],[0,1,1],[4,3,4]] #Création de la matrice
 
 
 
 
 
 def reduit(mat,ligne,colonne):
+    mat2 = [[0,0],[0,0]]
 # La fonction va parcourir la matrice, I verticalement et J horizontalement.
+    ml = 0
     for i in range 3:
+        mc = 0
+        if i != ligne:
         for j in range 3:
+            if j != colonne:
+                mat2[ml],[mc] = mat[l],[c]
+                mc += 1
+        ml += 1
+return mat2
