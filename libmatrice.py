0edd90b6a0e7ab2cc91990165c48ea41030e4523
Olivier Verrier <olivier.verrier@ynov.com>
2018-11-28 16:25:33 +0100

    Remise en place de det3

--- libmatrice.py
+++ libmatrice.py
@@ -65,8 +65,12 @@ On utilise également la fonction reduit(mat,ligne,colonne) afin d'obtenir la ma
 permettant de calculer son déterminant.
 
 '''
-
-
+def det3(mat):
+    #Condition taille 
+    if len(mat)!=3 or len(mat[0])!=3 or len(mat[1])!=3 or len(mat[2])!=3:
+        return 'error'
+    determ3 = mat[0][0] * reduit(mat,0,0) - mat[0][1] * reduit(mat,0,1) + mat[0][2] * reduit(mat,0,2)
+    return determ3
 
 
 
